e8255617a98ccddda1e315d20015cdd01c6e7a06
AmelaVorgic <62605720+AmelaVorgic@users.noreply.github.com>
2020-04-30 11:29:02 +0200

    Zadatak 6

--- IPasswordValidatorService.cs
+++ IPasswordValidatorService.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,4 +10,4 @@ namespace LV4RPPOON
     {
         bool IsValidPassword(String candidate);
     }
-}
+}
